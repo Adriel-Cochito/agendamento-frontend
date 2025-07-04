4efc00043f20c5ee1151e825aaaefc939f74fcb5
Adriel Cochito <adrielcochito.dev@gmail.com>
2025-07-03 22:34:31 -0300

    Update Home.tsx
    Titulo AgendaSim

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,7 +29,7 @@ export function Home() {
                 <div className="w-10 h-10 bg-gradient-to-br from-primary-500 to-primary-600 rounded-xl shadow-md flex items-center justify-center">
                   <Calendar className="w-6 h-6 text-white" />
                 </div>
-                <h1 className="text-xl font-bold text-gray-900">AgendaPro</h1>
+                <h1 className="text-xl font-bold text-gray-900">AgendaSim</h1>
               </div>
             </div>
 
